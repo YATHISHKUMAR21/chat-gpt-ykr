a77eba68c3e975f31b44ab909ccdbc21e899404b
YATHISHKUMAR21 <appuaishu5@gmail.com>
2026-03-03 21:47:21 +0530

    added authentication and user models

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 //dec 31 
+require('dotenv').config();
+const app = require('./src/app');
+const connectDB = require('./src/db/db');
+
+// Connect to MongoDB
+connectDB();
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
