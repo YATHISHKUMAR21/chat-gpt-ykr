dd4ec2b3105d6454af650a10a3405eb3b86b66c9
YATHISHKUMAR21 <appuaishu5@gmail.com>
2026-03-23 21:44:00 +0530

    addded path

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const path = require('path');
 
-// routes
+
+/* Routes */
 const authRoutes = require('./routes/auth.routes');
-const chatRoutes = require('./routes/chat.routes.js');
+const chatRoutes = require("./routes/chat.routes");
+
 
 const app = express();
 
-// middleware
-app.use(cookieParser());
-app.use(express.json());
 
-// ✅ Serve frontend (VERY IMPORTANT)
+
+/* using middlewares */
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// ✅ API routes
+
+
+/* Using Routes */
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
-// ✅ React fallback (VERY IMPORTANT)
-app.get("*", (req, res) => {
+
+app.get("*name", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
