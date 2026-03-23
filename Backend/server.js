ff42586f98f40c85e2150056af18a530f83cf11f
YATHISHKUMAR21 <appuaishu5@gmail.com>
2026-03-23 22:01:38 +0530

    refactored

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,15 @@
-//dec 31 
-require('dotenv').config();
-const app = require('./src/app');
-const connectDB = require('./src/db/db');
-const initSocketServer = require('./src/sockets/socket.server');  
-const httpServer = require('http').createServer(app);
+require("dotenv").config();
+const app = require("./src/app")
+const connectDb = require("./src/db/db");
+const initSocketServer = require("./src/sockets/socket.server");
+const httpServer = require("http").createServer(app);
 
-// Connect to MongoDB
-connectDB();
+
+
+connectDb()
 initSocketServer(httpServer);
 
-httpServer.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
 
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
